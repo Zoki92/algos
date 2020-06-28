fa0d681aaf7445998199aa2a9a7df88c0096145d
Zoki92 <zoranstoilov@yahoo.com>
2020-06-28 02:02:58 +0100

    Added stack

--- stack.js
+++ stack.js
@@ -1,1 +1,35 @@
-// this is next
+class Stack {
+    constructor() {
+        this.stack = [];
+        this.count = 0;
+    }
+
+    push(item) {
+        this.stack.push(item);
+        this.count++;
+    }
+
+    pop() {
+        if (isEmpty()) return;
+        this.count--;
+        return this.stack.pop()
+    }
+
+    peek() {
+        if (isEmpty()) return;
+        return this.stack[this.count - 1];
+    }
+
+    isEmpty() {
+        return this.count === 0;
+    }
+
+    size() {
+        return this.count;
+    }
+
+    clear() {
+        this.stack = [];
+        this.count = 0;
+    }
+}

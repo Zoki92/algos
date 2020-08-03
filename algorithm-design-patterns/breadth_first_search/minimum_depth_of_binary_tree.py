1b946be4aff3bb06903df63b828401c48f8e778c
Zoran <zoranstoilov@yahoo.com>
2020-08-03 19:35:03 +0100

    Minimum depth of binary tree

--- algorithm-design-patterns/breadth_first_search/minimum_depth_of_binary_tree.py
+++ algorithm-design-patterns/breadth_first_search/minimum_depth_of_binary_tree.py
@@ -1,5 +1,6 @@
 """
-Given a binary tree, populate an array to represent the averages of all of its levels.
+Find the minimum depth of a binary tree.
+The minimum depth is the number of nodes along the shortest path from the root node to the nearest leaf node.
 """
 from collections import deque
 
@@ -14,48 +15,38 @@ class TreeNode:
 # The space comlexity is O(n) as we need to return a list containing the level order traversal. We will also
 # need O(n) space for the queue. Since we can have maximum of N/2 nodes at any level (this can happen only at lowest level)
 # therefore we will need O(n) space to store them in the queue.
-def find_level_averages(root):
-    result = []
+def find_minimum_depth(root):
     if not root:
-        return result
+        return 0
+
     queue = deque()
     queue.append(root)
+    minimum_depth = 0
     while queue:
-        queue_length = len(queue)
-        level_sum = 0.0
-        for _ in range(queue_length):
+        minimum_depth += 1
+        level_size = len(queue)
+        for _ in range(level_size):
             current_node = queue.popleft()
-            level_sum += current_node.val
+
+            if not current_node.left and not current_node.right:
+                return minimum_depth
+
             if current_node.left:
                 queue.append(current_node.left)
             if current_node.right:
                 queue.append(current_node.right)
-        result.append(level_sum / queue_length)
-    return result
-
-    return result
 
 
 def main():
     root = TreeNode(12)
     root.left = TreeNode(7)
     root.right = TreeNode(1)
-    root.left.left = TreeNode(9)
-    root.left.right = TreeNode(2)
     root.right.left = TreeNode(10)
     root.right.right = TreeNode(5)
-    print("Level averages are: " + str(find_level_averages(root)))
+    print("Tree Minimum Depth: " + str(find_minimum_depth(root)))
+    root.left.left = TreeNode(9)
+    root.right.left.left = TreeNode(11)
+    print("Tree Minimum Depth: " + str(find_minimum_depth(root)))
 
 
 main()
-
-
-"""
-Similar Problems #
-
-Problem 1: Find the largest value on each level of a binary tree.
-
-Solution: We will follow a similar approach, but instead of having a running sum we will track the maximum value of each level.
-
-maxValue = max(maxValue, currentNode.val)
-"""

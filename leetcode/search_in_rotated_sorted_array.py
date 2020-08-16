8011290e72b9fc3ee4fdec7a9db571e2160e265a
Zoran <zoranstoilov@yahoo.com>
2020-08-16 20:34:04 +0100

    Search in rotated sorted array

---
+++ leetcode/search_in_rotated_sorted_array.py
@@ -0,0 +1,63 @@
+"""
+Given an integer array nums sorted in ascending order, and an integer target.
+
+Suppose that nums is rotated at some pivot unknown to you beforehand (i.e., [0,1,2,4,5,6,7] 
+might become [4,5,6,7,0,1,2]).
+
+You should search for target in nums and if you found return its index, otherwise return -1.
+
+ 
+
+Example 1:
+
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+
+Example 3:
+
+Input: nums = [1], target = 0
+Output: -1
+
+"""
+
+
+import pytest
+from typing import List
+
+
+def search(nums: List[int], target: int) -> int:
+
+    start, end = 0, len(nums) - 1
+    while start <= end:
+        mid = (start + end) // 2
+        if nums[mid] == target:
+            return mid
+
+        if nums[mid] >= nums[start]:
+            if target >= nums[start] and target <= nums[mid]:
+                end = mid - 1
+            else:
+                start = mid + 1
+        else:
+            if target > nums[mid] and target < nums[end]:
+                start = mid + 1
+            else:
+                end = mid - 1
+    return -1
+
+
+test_data = [
+    ([4, 5, 6, 7, 0, 1, 2], 0, 4),
+    ([4, 5, 6, 7, 0, 1, 2], 3, -1),
+    ([1], 0, -1),
+]
+
+
+@pytest.mark.parametrize("nums, target,expected", test_data)
+def test_search(nums, target, expected):
+    assert search(nums, target) == expected
